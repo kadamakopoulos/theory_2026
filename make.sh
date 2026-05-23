3d4812fa1dd1aeabba430ee67ae2315ed3a96190
Konstantinos Adamakopoulos <110929671+kadamakopoulos@users.noreply.github.com>
2026-05-24 00:00:03 +0300

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#dimiourgia tou parser apo to arxeio parser.y me xrisi tou bison
 bison -d -v -r all parser.y
+
+#dimiourgia tou lexer apo to arxeio mylexer.l me xrisi tou flex
 flex mylexer.l
+
+# meteglottisi olwn twn arxeiwn kai dimiourgia tou ektelesimou mycompiler
 gcc -o mycompiler lex.yy.c parser.tab.c cgen.c -lfl -lm
